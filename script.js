66a064df309b73ebd51e926c27265df73236d09c
Ravi Teja Karumuri <infinite007@Ravis-MacBook-Pro.local>
2023-12-19 00:42:12 -0700

    SPA for personal website

---
+++ script.js
@@ -0,0 +1,26 @@
+function loadContent(section) {
+    const contentDiv = document.getElementById('content');
+    switch(section) {
+        case 'home':
+            contentDiv.innerHTML = `<h1>Welcome to My Personal Website</h1>`;
+            break;
+        case 'resume':
+            contentDiv.innerHTML = `<h1>Resume</h1><p>Details of your professional experience and education.</p>`;
+            break;
+        case 'projects':
+            contentDiv.innerHTML = `<h1>Projects</h1><p>Details of your projects.</p>`;
+            break;
+        case 'skills':
+            contentDiv.innerHTML = `<h1>Skills</h1><p>Details of your skills.</p>`;
+            break;
+        case 'contact':
+            contentDiv.innerHTML = `<h1>Contact</h1><p>Your contact information.</p>`;
+            break;
+        default:
+            contentDiv.innerHTML = `<h1>Welcome</h1>`;
+    }
+}
+
+// Initially load the home content
+loadContent('home');
+
