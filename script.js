c7910d2db2416e5e0fdd5ad212f95b8df9d3d409
Ravi Teja Karumuri <infinite007@Ravis-MacBook-Pro.local>
2023-12-19 00:55:26 -0700

    another template

--- script.js
+++ script.js
@@ -2,25 +2,24 @@ function loadContent(section) {
     const contentDiv = document.getElementById('content');
     switch(section) {
         case 'home':
-            contentDiv.innerHTML = `<h1>Welcome to My Personal Website</h1>`;
+            contentDiv.innerHTML = '<h1>Welcome to My Personal Website</h1>';
             break;
         case 'resume':
-            contentDiv.innerHTML = `<h1>Resume</h1><p>Details of your professional experience and education.</p>`;
+            contentDiv.innerHTML = '<h1>Resume</h1><p>[Your resume details]</p>';
             break;
         case 'projects':
-            contentDiv.innerHTML = `<h1>Projects</h1><p>Details of your projects.</p>`;
+            contentDiv.innerHTML = '<h1>Projects</h1><p>[Details of your projects]</p>';
             break;
         case 'skills':
-            contentDiv.innerHTML = `<h1>Skills</h1><p>Details of your skills.</p>`;
+            contentDiv.innerHTML = '<h1>Skills</h1><p>[List of your skills]</p>';
             break;
         case 'contact':
-            contentDiv.innerHTML = `<h1>Contact</h1><p>Your contact information.</p>`;
+            contentDiv.innerHTML = '<h1>Contact</h1><p>[Your contact information]</p>';
             break;
         default:
-            contentDiv.innerHTML = `<h1>Welcome</h1>`;
+            contentDiv.innerHTML = '<h1>Welcome</h1>';
     }
 }
 
-// Initially load the home content
-loadContent('home');
+loadContent('home'); // Load home content initially
 
